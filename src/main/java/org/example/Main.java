86dbbd43215d79817ba1039c01ccf7a20c9d8135
phoenix-of-dawn <eliobenoit@live.com>
2025-04-29 15:34:23 -0400

    Added ascending/descending choice to bubblesort

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,22 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] nums = {3, 1, 5, 2, 1, 4};
-        insertionSort(nums);
+        bubbleSort(nums, false);
         System.out.println(Arrays.toString(nums));
     }
 
     public static void bubbleSort(int[] nums) {
+        bubbleSort(nums, true);
+    }
+
+    public static void bubbleSort(int[] nums, boolean ascending) {
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = 0; j < nums.length - 1 - i; j++) {
-                if (nums[j] > nums[j + 1]) {
+                boolean condition = ascending ?
+                        nums[j] > nums[j + 1] :
+                        nums[j] < nums[j + 1];
+
+                if (condition) {
                     int tmp = nums[j + 1];
                     nums[j + 1] = nums[j];
                     nums[j] = tmp;
